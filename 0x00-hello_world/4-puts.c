d85cf388548fd4e9aa7e1e7d744c6410b85aee44
Mims <enyata@Mimis-MacBook-Air.local>
2022-09-08 16:21:28 +0000

    Hello,Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,2 @@
-include <stdio.h>
-
-/**
-*main - print the string in the put funtion
-*
-*Description: using the main function
-*this program prints "Programming is like building a multilingual puzzle
-*Return: 0
-*/
-int main(void)
-{
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-}
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);	return (0);
