9dbe20ec7106175aadd134283aec5ffad61fa269
Mims <enyata@Mimis-MacBook-Air.local>
2022-09-08 19:15:55 +0000

    Hello Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-char str[] = "\"Programming is like building a multilingual puzzle";
-puts(str);	return (0);
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
