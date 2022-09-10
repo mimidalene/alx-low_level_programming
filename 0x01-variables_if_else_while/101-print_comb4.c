4d48ae8edb6e388509fe8b2b70060ad5f36f2f98
Mims <enyata@Mimis-MacBook-Air.local>
2022-09-10 16:17:27 +0000

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 
-			/**
-			 * main - print possible combo of 3
-			 *
-			 * Return: returns zero at the end
-			 */
-			int main(void)
+/**
+*main - entry point for code
+*Description - prints out every combination of 3 digits
+*Return: should return a value of 0
+*
+*/
+
+int main(void)
+{
+	int n;
+	int m;
+	int o;
+
+	for (n = 0; n < 9; n++)
+	{
+		for (m = n + 1; m <= 9; m++)
+		{
+			for (o = m + 1; o <= 9; o++)
 			{
-				int i, p, m;
-
-				for (i = 0; i <= 9; p++)
+				putchar('0' + n);
+				putchar('0' + m);
+				putchar('0' + o);
+				if (n < 7)
 				{
-				       for (p = 1; p <= 9; p++)
-				       {
-					      for (m = 2; m <= 9; m++)
-					      {
-					      if (m > p && p > i)
-					      {
-
-						      putchar(i + '0');
-						      putchar(p + '0');
-						      putchar(m + '0');
-							     if (i != 7)
-						      {
-							     putchar(',');
-							     putchar(' ');
-						      }
-						}
-						}
-					}
+					putchar(',');
+					putchar(' ');
 				}
-				putchar('\n');
-				return (0);
 			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
