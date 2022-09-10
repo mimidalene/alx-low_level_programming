b4786268952ded03ccd7d28702a83dc3a82f6d04
Mims <enyata@Mimis-MacBook-Air.local>
2022-09-10 16:24:23 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,101 +1,33 @@
-#include <stdio.h>
-
+#include<stdio.h>
 
 /**
-
-
- * main - print possible combination of two 2-digit
-
-
- * numbers
-
-
- *
-
-
- * Return: returns zero at the end
-
-
+ * main - prints combinations of two two digit numbers
+ * Return: returns 0 (success)
  */
-
-
 int main(void)
-
-
 {
+	int x;
+	int y;
 
-
-	int i, p;
-
-
-
-
-
-
-
-	for (i = 0; i < 100; i++)
-
-
+	for (x = 0; x < 100; x++)
 	{
-
-
-		for (p = 0; p < 100; j++)
-
-
+		for (y = 0; y < 100; y++)
 		{
-
-
-			if (p > i)
-
-
+			if (x < y)
 			{
-
-
-				putchar((i / 10) + '0');
-
-
-				putchar((i % 10) + '0');
-
-
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
 				putchar(' ');
-
-
-				putchar((p / 10) + '0');
-
-
-				putchar((p % 10) + '0');
-
-
-				if (i != 98)
-
-
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+				if (x != 98 || y != 99)
 				{
-
-
 					putchar(',');
-
-
 					putchar(' ');
-
-
 				}
-
-
 			}
-
-
 		}
-
-
 	}
-
-
 	putchar('\n');
-
-
 	return (0);
-
-
 }
-
-
