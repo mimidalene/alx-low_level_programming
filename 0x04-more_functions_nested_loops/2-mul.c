1bd2de47aa01aad022d7cf320b27971ccc12faf5
Mims <enyata@Mimis-MacBook-Air.local>
2022-09-15 17:18:38 +0000

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
 *mul - multiplies two integers
-*@a: integer 1
-*@b: integer 2
-*Return: return the value
-*
+*@a: first number
+*@b: second number
+*Return: returns result
 */
 
 int mul(int a, int b)
