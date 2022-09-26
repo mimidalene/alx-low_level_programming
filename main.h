81e4ddd95f597edc75594f46953d4255107de01a
Mims <enyata@Mimis-MacBook-Air.local>
2022-09-26 13:08:10 +0000

    0-memset.c

--- main.h
+++ main.h
@@ -1,23 +1,18 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-/**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+#include <stddef.h>
 
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-int _putchar(char);
-int _isupper(int);
-int _isdigit(int);
-int mul(int, int);
+int _putchar(char c);
 
-#endif /* MAIN_H */
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
+
+#endif
