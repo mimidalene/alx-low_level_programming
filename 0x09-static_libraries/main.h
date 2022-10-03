45bb41234ecfc40f7d5773bdcb10a2f8a5b6add7
Mims <enyata@Mimis-MacBook-Air.local>
2022-10-03 13:20:11 +0000

    0-whatsmyname.c

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -18,7 +18,7 @@ int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsigned int  int_strspn(char *s, char *accept);
+unsigned int int_strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
