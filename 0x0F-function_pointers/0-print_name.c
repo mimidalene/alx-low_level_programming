4752b25dcf6c1519c0fd37b7844e349ca36ac36a
Mims <enyata@Mimis-MacBook-Air.local>
2022-10-13 16:43:21 +0000

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,20 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 /**
- * print_name - prints name
- * @name: pointer to text
- * @f: function pointer
- * Return: nothing
- */
+  * print_name - prints the name
+  *@name: name to be printed
+  *@f: pointer to the function
+  *Return: Null
+  *
+  */
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	if (f != NULL)
+	{
+		if (name != NULL)
+		{
+			(*f)(name);
+		}
+	}
 }
