c1f5690723ad4b45df99604f4641dfaa639cbaf9
Mims <enyata@Mimis-MacBook-Air.local>
2022-10-13 17:46:15 +0000

    0-sum_them_all.c

--- 0x0F-function_pointers/0x10-variadic_functions/0-sum_them_all.c
+++ 0x0F-function_pointers/0x10-variadic_functions/0-sum_them_all.c
@@ -1,30 +1,27 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
-
 /**
- * sum_them_all - function to add all of its arguments
- * @n: the number of arguments
+ * sum_them_all - function that sum of all its parameters
  *
- * Return: the sum of all parameters
+ * @n: How many variables will be inserted in the function
+ *
+ * Return: return the sum of all its parameters
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i = 0;
-	int sum = 0;
+	va_list list;
+	unsigned int i, sum = 0;
+
+	va_start(list, n);
 
 	if (n == 0)
-		return (0);
-
-	va_start(ap, n);
-
-	for (; i < n; i++)
 	{
-		sum += va_arg(ap, int);
+		return (0);
+	}
+	for (i = 0; i < n; i++)
+	{
+		sum = sum + va_arg(list, int);
 	}
 
-	va_end(ap);
-
+	va_end(list);
 	return (sum);
 }
--- 0x0F-function_pointers/0x10-variadic_functions/variadic_functions.h
+++ 0x0F-function_pointers/0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,12 @@
-#ifndef VARIADIC_FUNCTION_HEADER
-#define VARIADIC_FUNCTION_HEADER
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-int _putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-#endif /* VARIADIC_FUNCTION_HEADER */
+#endif
